047f5ce131874a3832f082db19c0a94bfbef1e79
SARVJEETYADUVANSHI1998 <sarvjeetyaduvanshi1998@gmail.com>
2021-05-03 20:27:43 +0530

    deploy

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -5,6 +5,7 @@ const About = () => {
     return (
         <div className="div">
             <h1>Hi! i am mr jeetu </h1>
+            <img src="https://unsplash.com/photos/4o71vkGV9J0" className="Pttt"/>
         </div>
     )
 }
